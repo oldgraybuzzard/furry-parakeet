c99b91f214da8593e0c6b89c30f999fd3f6f0a7c
oldgraybuzzard <k_felder@me.com>
2021-09-10 22:27:07 -0400

    code still in the works

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-// Assignment code here
-var passwordLength = window.prompt('Password Length?');
-console.log('Password length = ' + passwordLength);
-var passwordSpecialCharacters = window.confirm('Do you want special characters?');
-console.log("Special Characters = " + passwordSpecialCharacters);
-var passwordNumbers = window.confirm('Do you want to add numbers?');
-console.log('Use numbers = ' + passwordNumbers);
 
-let specialCharacters = [
-  '!', '@', '#', ')', '(', '*', '&', '~', '-', '+', '.', '_', '{', '|', '?', ']', '[', '%', ';', '"'
-];
 
-//Generator functions
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function generatePassword(passwordLength) {
+    var passwordLength = prompt('Password Length?');
+    var passwordSpecialCharacters = confirm('Do you want special characters?');
+    var passwordNumbers = confirm('Do you want to add numbers?');
+    var createdPassword = "";
+   
+var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*()";
+var allTogether = letters + numbers + symbols;
+var randomArray = Array(passwordLength);
+  randomPasswordArray[0] = numbers;
+  randomPasswordArray[1] = letters;
+  randomPasswordArray[2] = symbols;
+  randomPasswordArray = randomPasswordArray.fill(allTogether, 3);
+  return shuffleArray(randomPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
 }
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
 }
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = specialCharacters;
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
