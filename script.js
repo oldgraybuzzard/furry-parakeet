3422463ca87ee11b5e4a01fd4e921bb6c90ee5ab
oldgraybuzzard <k_felder@me.com>
2021-09-12 19:22:55 -0400

    refactored code

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-
-function generatePassword() {
-  var length = prompt('Enter a password length in numeric form, between 8 and 128!');
-      while (length < 8 || length > 128) {
-      if (length < 8 || length > 128) {
-        alert("ERROR: Password does not meet requirement. Minimum of 8 characters, max of 128. Please try again!");
-        var length = prompt('Choose a password length with a minimum of 8 characters and a max of 128 characters!');
-     }
-  };
-  var addSpecialCharacters = confirm('Do you want special characters?');
-  var addNumbers = confirm('Do you want to add numbers?');
-  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+  var string = "abcdefghijklmnopqrstuvwxyz";
+  var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var passwordNumbers = '0123456789';
   var symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
   var password = "";
   var character = "";
-  var crunch = true;
-  while(password.length<length) {
-      char1 = Math.floor(string.length * Math.random()*Math.random());
-      char2 = Math.floor(passwordNumbers.length * Math.random()*Math.random());
-      char3 = Math.floor(symbols.length * Math.random()*Math.random());
-      hold = string.charAt(char1);
-      hold = (password.length%0==0)?(hold.toUpperCase()):(hold);
-      character += hold;
+
+function generatePassword() {
+  debugger;
+  var passLength = prompt('Enter a password length in numeric form, between 8 and 128!');
+      while (passLength < 3 || passLength > 128) {
+      //if (passLength < 8 || passLength > 128) {
+        alert("ERROR: Password does not meet requirement. Minimum of 8 characters, max of 128. Please try again!");
+        var passLength = prompt('Choose a password length with a minimum of 8 characters and a max of 128 characters!');
+     //}
+  };
+  var addSpecialCharacters = confirm('Do you want special characters?');
+  var addNumbers = confirm('Do you want to add numbers?');
+  var i = 0;
+
+  while(i != passLength) {
+    let choice = Math.floor(Math.random() * 4);
+
+    if (choice == 0) {
+      char1 = Math.floor(string.length * Math.random());
+      character = string.charAt(char1);
+    } 
+    
+    if (choice == 1) {
       if (addNumbers === true) {
-          character += passwordNumbers.charAt(char2);
-      };
+        char2 = Math.floor(passwordNumbers.length * Math.random());
+        character = passwordNumbers.charAt(char2);
+      } else {
+        char1 = Math.floor(string.length * Math.random());
+        character = string.charAt(char1);
+      }
+    } 
+    
+    if (choice == 2) {
       if (addSpecialCharacters === true) {
-         character += symbols.charAt(char3);
-      };
-      password = character;
+        char3 = Math.floor(symbols.length * Math.random());
+        character = symbols.charAt(char3);
+      } else {
+        char1 = Math.floor(string.length * Math.random());
+        character = string.charAt(char1);
+      }
+    }
+
+    if (choice == 3) {
+      char1 = Math.floor(stringUpper.length * Math.random());
+      character = stringUpper.charAt(char1);
+    } 
+  
+    password += character;
+    i++;
+    
   }
-  password=password.split('').sort(function(){return Math.random()}).join('');
+
+  //password=password.split('').sort(function(){return Math.floor(Math.random())}).join('');
   return password;
 }
 
